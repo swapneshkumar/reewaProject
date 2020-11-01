f0bda48398607a4396381fccdf34ca51ad5a0f70
swapneshkumar <pal.swapnesh1989@yahoo.in>
2020-11-01 16:17:32 +0530

    initial checkin for node express project

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "mydb",
-  insecureAuth : true,
-
+  HOST: "us-cdbr-iron-east-02.cleardb.net",
+  USER: "b7e24378878xxx",
+  PASSWORD: "0200exxx",
+  DB: "heroku_7643ec736354xxx"
 };
